119d1bdf013661a3ea85e175ec9d3e8e74dbbdf3
Brian McKinney <brian@realitycontainment.com>
2010-04-02 10:55:52 -0600

    Rails3 beta2 did away with railtie_name

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -2,10 +2,9 @@ require 'action_dispatch'
 require 'rails'
 
 class Railtie < Rails::Railtie
- railtie_name :redis_session_store
 
- initializer 'redis-store.load_session_store' do |app|
-   require 'middleware/session/redis_store'
+  initializer 'redis-store.load_session_store' do |app|
+    require 'middleware/session/redis_store'
   end
 
 end
